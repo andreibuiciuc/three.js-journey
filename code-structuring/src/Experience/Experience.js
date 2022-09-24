0cc02a1a71ff1d63d590d07f9757a1e984f3a093
Andrei Buiciuc <abuiciuc33@gmail.com>
2022-09-24 23:53:50 +0300

    added dispose method

--- code-structuring/src/Experience/Experience.js
+++ code-structuring/src/Experience/Experience.js
@@ -62,4 +62,27 @@ export default class Experience {
         this.world.update();
         this.renderer.update();
     }
+
+    destroy() {
+        this.sizes.off('resize');
+        this.time.off('tick');
+        this.scene.traverse(child => {
+            if (child instanceof THREE.Mesh) {
+                child.geometry.dispose();
+
+                for(const key in child.material) {
+                    const value = child.material[key];
+                    if (value && typeof value.dispose === 'function') {
+                        value.dispose();
+                    }
+                }
+            }
+        });
+        this.camera.controls.dispose();
+        this.renderer.instance.dispose();
+        if (this.debug.isActive) {
+            this.debug.gui.destroy();
+        }
+
+    }
 }
